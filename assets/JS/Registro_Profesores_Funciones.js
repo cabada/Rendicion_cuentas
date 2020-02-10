9364552f4576e9c3900de432946a20bb311c75bd
¨Cabada¨ <j_cabada@hotmail.com>
2020-02-09 17:23:44 -0700

    Se agregaron los ids de los componentes y ademas se agrego el archivo JS en el head.

--- assets/JS/Registro_Profesores_Funciones.js
+++ assets/JS/Registro_Profesores_Funciones.js
@@ -1,10 +1,14 @@
-function agregarDatos(nombre_curso,periodo,horas_capacitacion,numero_participantes_base,
-                      numero_participantes_honorarios) {
-    cadena = "nombre_curso=" + nombre_curso +
-        "&periodo=" + periodo +
-        "&horas_capacitacion=" + horas_capacitacion +
-        "&numero_participantes_base=" + numero_participantes_base +
-        "&numero_participantes_honorarios=" + numero_participantes_honorarios;
+function agregarDatos(nombre_completo,sexo,grado_estudios,horas_jornada,area_academica,
+                      disciplina,vigencia,area_experiencia,fecha_ingreso) {
+    cadena = "nombre_completo=" + nombre_completo +
+        "&sexo=" + sexo +
+        "&grado_estudios=" + grado_estudios +
+        "&horas_jornada=" + horas_jornada +
+        "&area_academica=" + area_academica +
+        "&disciplina" + disciplina +
+        "&vigencia" + vigencia +
+        "&area_experiencia" + area_experiencia +
+        "&fecha_ingreso" + fecha_ingreso;
 
     $.ajax({
         type:"post",
