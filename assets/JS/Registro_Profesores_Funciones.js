ca61299109822ed81273e327b643d412a1bd834d
¨Cabada¨ <j_cabada@hotmail.com>
2020-02-09 21:31:36 -0700

    Agregada la ruta de agregar profesores y la tabla de profesores.

--- assets/JS/Registro_Profesores_Funciones.js
+++ assets/JS/Registro_Profesores_Funciones.js
@@ -12,11 +12,11 @@ function agregarDatos(nombre_completo,sexo,grado_estudios,horas_jornada,area_aca
 
     $.ajax({
         type:"post",
-        url:"php/agregarDatos.php",
+        url:"../componentes/PHP_Consultas/Registro_Profesores/Agregar_Registro.php",
         data:cadena,
         success:function(r) {
             if(r==1){
-                $('#tablaRegistroCurso').load('componentes/TablaRegistroCurso.php');
+                $('#registro_profesores').load('../componentes/registro_profesores.php');
                 alertify.success("Agregado con exito: ");
             }
             else{
