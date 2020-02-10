e4d9457fc96780ac9726d710155f48df38d2bd83
¨Cabada¨ <j_cabada@hotmail.com>
2020-02-09 17:35:53 -0700

    Se agrego la consulta sql para agregar datos a la tabla profesores.

--- assets/components/PHP_Consultas/Registro_Profesores/Agregar_Registro.php
+++ assets/components/PHP_Consultas/Registro_Profesores/Agregar_Registro.php
@@ -1,5 +1,36 @@
 <?php
 
+require_once "../Conexion.php";
+$conexion = conexion();
 
+$nombre_completo=$_POST['nombre_completo'];
+$sexo=$_POST['sexo'];
+$grado_estudios=$_POST['grado_estudios'];
+$horas_jornada=$_POST['horas_jornada'];
+$area_academica=$_POST['area_academica'];
+$disciplina = $_POST['disciplina'];
+$vigencia = $_POST['vigencia'];
+$area_experiencia = $_POST['area_experiencia'];
+$fecha_ingreso = $_POST['fecha_ingreso'];
+
+$stmt = $conexion->prepare("insert into profesores (
+                                        nombre_completo,
+                                        sexo,
+                                        grado_estudios,
+                                        hora_jornada,
+                                        id_area_academica,
+                                        id_disciplina,
+                                        vigencia,
+                                        id_area_experiencia,
+                                        fecha_ingreso
+                                        )
+                                        values (?,?,?,?,?,?,?,?,?)");
+$stmt->bind_param("sssiiisis",$nombre_completo,$sexo, $grado_estudios,
+    $horas_jornada,$area_academica,$disciplina,$vigencia,$area_experiencia,$fecha_ingreso);
+
+echo $resultado = $stmt->execute();
+
+$stmt->close();
+$conexion->close();
 
 ?>
