3f7a90472debd803c597d4d5261e040f0b4c235e
¨Cabada¨ <j_cabada@hotmail.com>
2020-03-15 22:12:58 -0700

    Se agregaron funciones y queries para actualizar registros.

---
+++ assets/components/PHP_Consultas/Registro_Equipo_Maetsros_ITCJ/Actualizar_Registro.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once "../Conexion.php";
+$conexion = conexion();
+
+$id_equipo_ms=$_POST['id_equipo_ms'];
+$nombre_docente=$_POST['nombre_docente'];
+$categoria=$_POST['categoria'];
+$grado_estudios=$_POST['grado_estudios'];
+$sni=$_POST['sni'];
+$area_especializacion=$_POST['area_especializacion'];
+$experiencia_profesional=$_POST['experiencia_profesional'];
+$experiencia_docente=$_POST['experiencia_docente'];
+
+$stmt = $conexion->prepare("update equipo_maestros_itcj set
+                                   id_equipo_maestros_itcj=?,
+                                   nombre_docente=?,
+                                   categoria_hora=?,
+                                   grado_estudios=?,
+                                   sni=?,
+                                   area_especializacion=?,
+                                   experiencia_profesional=? 
+                                   where id_equipo_maestros_itcj=$id_equipo_ms");
+
+$stmt->bind_param("sssssii", $nombre_docente,$categoria,$grado_estudios,$sni,$area_especializacion,$experiencia_profesional,$experiencia_docente );
+
+echo $resultado = $stmt->execute();
+$stmt->close();
+$conexion->close();
