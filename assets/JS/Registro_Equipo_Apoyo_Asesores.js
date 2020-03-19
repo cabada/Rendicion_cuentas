bbb62cce2cdc4aea077a7e0f30fa15f09369d2cc
¨Cabada¨ <j_cabada@hotmail.com>
2020-03-18 23:25:19 -0700

    Se agregaron las funciones al JS.

--- assets/JS/Registro_Equipo_Apoyo_Asesores.js
+++ assets/JS/Registro_Equipo_Apoyo_Asesores.js
@@ -25,41 +25,33 @@ function agregaform(datos) {
 
     $('#id_editar').val(d[0]);
     $('#nombre_editar').val(d[1]);
-    $('#categoria_editar').val(d[2]);
+    $('#puesto_editar').val(d[2]);
     $('#grado_estudios_editar').val(d[3]);
-    $('#sni_editar').val(d[4]);
-    $('#area_especializacion_editar').val(d[5]);
-    $('#experiencia_profesional_editar').val(d[6]);
-    $('#experiencia_docente_editar').val(d[7]);
+    $('#funciones_tecnm_editar').val(d[4]);
 
 }
 
 function actualizarDatos() {
-    id_equipo_ms=$('#id_editar').val();
-    nombre_docente=$('#nombre_editar').val();
-    categoria=$('#categoria_editar').val();
+    id_equipo_apoyo_as=$('#id_editar').val();
+    nombre=$('#nombre_editar').val();
+    categoria=$('#puesto_editar').val();
     grado_estudios=$('#grado_estudios_editar').val();
-    sni=$('#sni_editar').val();
-    area_especializacion=$('#area_especializacion_editar').val();
-    experiencia_profesional=$('#experiencia_profesional_editar').val();
-    experiencia_docente=$('#experiencia_docente_editar').val();
+    funciones=$('#funciones_tecnm_editar').val();
 
-    cadena="id_equipo_ms=" + id_equipo_ms +
-        "&nombre_docente=" + nombre_docente +
-        "&categoria=" + categoria +
+
+    cadena="id_equipo_apoyo_as=" + id_equipo_apoyo_as+
+        "&nombre=" + nombre +
+        "&puesto=" + puesto +
         "&grado_estudios=" + grado_estudios +
-        "&sni=" + sni +
-        "&area_especializacion=" + area_especializacion +
-        "&experiencia_profesional=" + experiencia_profesional +
-        "&experiencia_docente=" + experiencia_docente;
+        "&funciones=" + funciones;
 
     $.ajax({
         type: "POST",
-        url: "assets/components/PHP_Consultas/Registro_Equipo_Maestros_ITCJ/Actualizar_Registro.php",
+        url: "assets/components/PHP_Consultas/Registro_Equipo_Apoyo_Asesores/Actualizar_Registro.php",
         data: cadena,
         success: function (r) {
             if (r == 1) {
-                $('#registro-equipo-maestros-itcj').load('assets/components/registro-equipo-maestros-itcj.php');
+                $('#registro-equipo-maestros-itcj').load('assets/components/registro-equipo-apoyo-asesores.php');
                 alertify.success("Actualizado con exito");
             } else {
                 alertify.error("Fallo el servidor");
@@ -69,22 +61,22 @@ function actualizarDatos() {
 
 }
 
-function preguntarSiNo(id_cuerpos_academicos_posgrado){
+function preguntarSiNo(id_equipo_apoyo_as){
 
     alertify.confirm('Eliminar este registro', 'Esta seguro de eliminar este registro?',
-        function(){ eliminarDatos(id_cuerpos_academicos_posgrado)}
+        function(){ eliminarDatos(id_equipo_apoyo_as)}
         , function(){ alertify.error('Se cancelo')});
 }
-function eliminarDatos(id_equipo_ms) {
-    cadena= "id_equipo_ms=" + id_equipo_ms;
+function eliminarDatos(id_equipo_apoyo_as) {
+    cadena= "id_equipo_apoyo_as=" + id_equipo_apoyo_as;
 
     $.ajax({
         type:"post",
-        url:"assets/components/PHP_Consultas/Registro_Equipo_Maestros_ITCJ/Eliminar_Registro.php",
+        url:"assets/components/PHP_Consultas/Registro_Equipo_Apoyo_Asesores/Eliminar_Registro.php",
         data:cadena,
         success:function (r) {
             if(r==1){
-                $('#registro-equipo-maestros-itcj').load('assets/components/registro-equipo-maestros-itcj.php');
+                $('#registro-equipo-maestros-itcj').load('assets/components/registro-equipo-apoyo-asesores.php');
                 alertify.success("Eliminado con exito!");
             }else{
                 alertify.error("Fallo el servidor!");
