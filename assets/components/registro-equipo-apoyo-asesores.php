37cfe5f96380f5d164319a2e2c3669f5a21cd641
¨Cabada¨ <j_cabada@hotmail.com>
2020-03-18 23:09:08 -0700

    Se agrego conexion al html.

--- assets/components/registro-equipo-apoyo-asesores.php
+++ assets/components/registro-equipo-apoyo-asesores.php
@@ -1,3 +1,11 @@
+<?php
+
+require_once "PHP_Consultas/Conexion.php";
+$conexion = conexion();
+
+?>
+
+
 <div class="row">
     <div class="col-sm-12">
         <h2>Registro equipo de apoyo de asesores</h2>
@@ -13,16 +21,42 @@
                     <td class="text-center align-middle background-table">Funciones TECNM</td>
                     <td class="text-center align-middle background-table">Acciones</td>
                 </tr>
+
+                <?php
+
+                $sql="select id_equipo_apoyo,nombre,puesto,grado_estudios,funciones_med_tecnm from equipo_apoypo_asesores_pda";
+
+                $resultado = mysqli_query($conexion,$sql);
+
+                while($buscar=mysqli_fetch_row($resultado)) {
+
+                $datos = $buscar[0]."||".
+                    $buscar[1]."||".
+                    $buscar[2]."||".
+                    $buscar[3]."||".
+                    $buscar[4]."||".
+                    $buscar[5]."||".
+                    $buscar[6]."||".
+                    $buscar[7];
+
+                ?>
+
                 <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td><?php echo $buscar[1]?></td>
+                    <td><?php echo $buscar[2]?></td>
+                    <td><?php echo $buscar[3]?></td>
+                    <td><?php echo $buscar[4]?></td>
                     <td class="text-center align-middle">
                         <button class="btn btn-sm btn-warning" data-toggle="modal" data-target="#modalEdicion"><i class="far fa-edit"></i>  Editar</button>
                         <button class="btn btn-sm btn-danger"><i class="fas fa-trash"></i>  Eliminar</button>
                     </td>
                 </tr>
+
+                    <?php
+
+                }
+                ?>
+
             </table>
         </div>
     </div>
