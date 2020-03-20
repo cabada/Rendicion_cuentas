6b1b1da65993b50710e0a59e60262cde555634b7
¨Cabada¨ <j_cabada@hotmail.com>
2020-03-19 19:56:05 -0700

    Se agregraron las funciones JS al HTML para la tabla evaluacion docente.

---
+++ assets/JS/Registro_Evaluacion_Docente.js
@@ -0,0 +1,90 @@
+function  agregarDatos(periodo,docentes_evaluados,porcentaje) {
+
+    cadena="periodo=" + periodo +
+        "&docentes_evaluados=" + docentes_evaluados +
+        "&porcentaje=" + porcentaje;
+
+    $.ajax({
+        type: "POST",
+        url: "assets/components/PHP_Consultas/Registro_Evaluacion_Docente/Agregar_Registro.php",
+        data: cadena,
+        success: function (r) {
+            if (r == 1) {
+                $('#registro-equipo-apoyo-asesores').load('assets/components/registro-evaluacion-docente.php');
+                alertify.success("Agregado con exito");
+            } else {
+                alertify.error("Fallo el servidor");
+            }
+        }
+    });
+}
+
+function agregaform(datos) {
+    d=datos.split('||');
+
+    $('#id_editar').val(d[0]);
+    $('#nombre_editar').val(d[1]);
+    $('#puesto_editar').val(d[2]);
+    $('#grado_estudios_editar').val(d[3]);
+    $('#funciones_tecnm_editar').val(d[4]);
+
+}
+
+function actualizarDatos() {
+    id_equipo_apoyo_as=$('#id_editar').val();
+    console.log(id_equipo_apoyo_as);
+    nombre=$('#nombre_editar').val();
+    console.log(nombre);
+    puesto=$('#puesto_editar').val();
+    console.log(puesto);
+    grado_estudios=$('#grado_estudios_editar').val();
+    console.log(grado_estudios);
+    funciones=$('#funciones_tecnm_editar').val();
+    console.log(funciones);
+
+
+    cadena="id_equipo_apoyo_as=" + id_equipo_apoyo_as+
+        "&nombre=" + nombre +
+        "&puesto=" + puesto +
+        "&grado_estudios=" + grado_estudios +
+        "&funciones=" + funciones;
+
+    $.ajax({
+        type: "POST",
+        url: "assets/components/PHP_Consultas/Registro_Equipo_Apoyo_Asesores/Actualizar_Registro.php",
+        data: cadena,
+        success: function (r) {
+            if (r == 1) {
+                $('#registro-equipo-apoyo-asesores').load('assets/components/registro-equipo-apoyo-asesores.php');
+                alertify.success("Actualizado con exito");
+            } else {
+                alertify.error("Fallo el servidor");
+            }
+        }
+    });
+
+}
+
+function preguntarSiNo(id_equipo_apoyo_as){
+
+    alertify.confirm('Eliminar este registro', 'Esta seguro de eliminar este registro?',
+        function(){ eliminarDatos(id_equipo_apoyo_as)}
+        , function(){ alertify.error('Se cancelo')});
+}
+function eliminarDatos(id_equipo_apoyo_as) {
+    cadena= "id_equipo_apoyo_as=" + id_equipo_apoyo_as;
+
+    $.ajax({
+        type:"post",
+        url:"assets/components/PHP_Consultas/Registro_Equipo_Apoyo_Asesores/Eliminar_Registro.php",
+        data:cadena,
+        success:function (r) {
+            if(r==1){
+                $('#registro-equipo-maestros-itcj').load('assets/components/registro-equipo-apoyo-asesores.php');
+                alertify.success("Eliminado con exito!");
+            }else{
+                alertify.error("Fallo el servidor!");
+            }
+        }
+    });
+}
