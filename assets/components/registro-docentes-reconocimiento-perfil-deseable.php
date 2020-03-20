f879bf9a32d843db1f030732e4669f01a5ca8335
iProxys <edwinavahh@outlook.com>
2020-03-19 23:12:16 -0600

    Agregando tablas html y php que me encargo Cabada.

---
+++ assets/components/registro-docentes-reconocimiento-perfil-deseable.php
@@ -0,0 +1,27 @@
+<div class="row">
+    <div class="col-sm-12">
+        <h2>Registro de docentes con reconocimiento perfil deseable</h2>
+        <caption>
+            <button class="btn btn-main" data-toggle="modal" data-target="#new-modal">Agregar registro  <i class="fas fa-plus"></i></button>
+        </caption>
+        <div class="table-responsive-xl">
+            <table class="table table-sm table-hover table-condensed table-bordered table-striped mt-2">
+                <tr>
+                    <td class="text-center align-middle background-table">Docente</td>
+                    <td class="text-center align-middle background-table">Área adscripción</td>
+                    <td class="text-center align-middle background-table">Vigencia</td>
+                    <td class="text-center align-middle background-table">Acciones</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td class="text-center align-middle">
+                        <button class="btn btn-sm btn-warning" data-toggle="modal" data-target="#modalEdicion"><i class="far fa-edit"></i>  Editar</button>
+                        <button class="btn btn-sm btn-danger"><i class="fas fa-trash"></i> Eliminar</button>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
