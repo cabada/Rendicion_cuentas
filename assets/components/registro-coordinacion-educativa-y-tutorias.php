edb25c95db0d274afef779244ca03b37be74f38c
ArelyAM <56061706+ArelyAM@users.noreply.github.com>
2020-03-19 23:04:05 -0600

    Funcion ver datos de tabla coordinacion educativa lista

--- assets/components/registro-coordinacion-educativa-y-tutorias.php
+++ assets/components/registro-coordinacion-educativa-y-tutorias.php
@@ -1,6 +1,13 @@
+<?php
+
+require_once "PHP_Consultas/Conexion.php";
+$conexion = conexion();
+
+?>
+
 <div class="row">
     <div class="col-sm-12">
-        <h2>Registro de coordinación educativa y tutorías</h2>
+        <h2>Registro de coordinación educativa</h2>
         <caption>
             <button class="btn btn-main" data-toggle="modal" data-target="#new-modal">Agregar registro  <i class="fas fa-plus"></i></button>
         </caption>
@@ -11,14 +18,26 @@
                     <td class="text-center align-middle background-table">Periodo</td>
                     <td class="text-center align-middle background-table">Acciones</td>
                 </tr>
+
+                <?php
+                     $sql="select id_actividad,nombre_actividad,periodo
+                            from coordinacion_educativa_y_tutorias";
+                     $result=mysqli_query($conexion,$sql);
+
+                     while($ver=mysqli_fetch_row($result)){
+                ?>
+
                 <tr>
-                    <td></td>
-                    <td></td>
+                    <td><?php echo $ver[1]?></td>
+                    <td><?php echo $ver[2]?></td>
                     <td class="text-center align-middle">
                         <button class="btn btn-sm btn-warning" data-toggle="modal" data-target="#modalEdicion"><i class="far fa-edit"></i>  Editar</button>
                         <button class="btn btn-sm btn-danger"><i class="fas fa-trash"></i>  Eliminar</button>
                     </td>
                 </tr>
+                <?php
+                     }
+                ?>
             </table>
         </div>
     </div>
