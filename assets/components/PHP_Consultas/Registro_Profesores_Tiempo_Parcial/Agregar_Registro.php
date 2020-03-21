8b3d1a39448eec71364a72ec1b1244a38f527d7e
¨Cabada¨ <j_cabada@hotmail.com>
2020-03-20 21:53:33 -0600

    Se agregaron los archivos php de las queries para agregar, eliminar y modificar la tabla profesores de tiempo parcial.

---
+++ assets/components/PHP_Consultas/Registro_Profesores_Tiempo_Parcial/Agregar_Registro.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once "../Conexion.php";
+$conexion = conexion();
+
+$cantidad_tiempo_parcial=$_POST['cantidad_tiempo_parcial'];
+$grado=$_POST['grado'];
+
+$stmt = $conexion->prepare("insert into profesores_tiempo_parcial (cantidad_tiempo_parcial,grado) values (?,?)");
+$stmt->bind_param("ssss", $cantidad_tiempo_parcial,$grado);
+
+echo $resultado = $stmt->execute();
+$stmt->close();
+$conexion->close();
+
+?>
+
