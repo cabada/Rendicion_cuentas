a8fa408aac5c05cdc422cd562405f8e2f68e7cf1
¨Cabada¨ <j_cabada@hotmail.com>
2020-03-20 22:49:51 -0600

    Se agregaron las funciones de agregar, actualizar y eliminar en el JS de la tabla total de profesores por grado academico.

--- assets/components/PHP_Consultas/Registro_Total_Profesores_GA/Actualizar_Registro.php
+++ assets/components/PHP_Consultas/Registro_Total_Profesores_GA/Actualizar_Registro.php
@@ -5,7 +5,7 @@ $conexion = conexion();
 
 $id_prof_grado_acad=$_POST['id_prof_grado_acad'];
 $grado=$_POST['grado'];
-$cantidad=$_POST['cantidad_'];
+$cantidad=$_POST['cantidad'];
 
 $stmt = $conexion->prepare("update total_profesores_grado_academico set
                                    grado=?,
