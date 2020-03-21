d55d7e25b7141104be89e0421e7664af59cecfad
¨Cabada¨ <j_cabada@hotmail.com>
2020-03-21 00:53:20 -0600

    Se agregaron los archivos de JS y se modifico el HTML de la tabla anio sabatico.

---
+++ assets/JS/Registro_Producto_Anio_Sabatico.js
@@ -0,0 +1,81 @@
+function  agregarDatos(profesor,proyecto_realizado) {
+
+    cadena="profesor=" + profesor +
+        "&proyecto_realizado=" + proyecto_realizado;
+
+    $.ajax({
+        type: "POST",
+        url: "assets/components/PHP_Consultas/Registro_Producto_Anio_Sabatico/Agregar_Registro.php",
+        data: cadena,
+        success: function (r) {
+            if (r == 1) {
+                $('#registro-anio-sabatico').load('assets/components/registro-anio-sabatico.php');
+                alertify.success("Agregado con exito");
+            } else {
+                alertify.error("Fallo el servidor");
+            }
+        }
+    });
+}
+
+function agregaform(datos) {
+    d=datos.split('||');
+
+    $('#id_prof_editar').val(d[0]);
+    $('#profesor_editar').val(d[1]);
+    $('#proyecto_realizado_editar').val(d[2]);
+
+}
+
+function actualizarDatos() {
+    id_sabatico=parseInt($('#id_prof_editar').val());
+    console.log(id_sabatico);
+    profesor= $('#profesor_editar').val();
+    console.log(profesor);
+    proyecto_realizado=$('#proyecto_realizado_editar').val();
+    console.log(proyecto_realizado);
+
+
+    cadena="id_sabatico="+id_sabatico+
+        "&profesor=" + profesor+
+        "&proyecto_realizado=" + proyecto_realizado;
+
+    $.ajax({
+        type: "POST",
+        url: "assets/components/PHP_Consultas/Registro_Producto_Anio_Sabatico/Actualizar_Registro.php",
+        data: cadena,
+        success: function (r) {
+            if (r == 1) {
+                $('#registro-anio-sabatico').load('assets/components/registro-anio-sabatico.php');
+                alertify.success("Actualizado con exito");
+            } else {
+                alertify.error("Fallo el servidor");
+            }
+        }
+    });
+
+}
+
+function preguntarSiNo(id_sabatico){
+
+    alertify.confirm('Eliminar este registro', 'Esta seguro de eliminar este registro?',
+        function(){ eliminarDatos(id_sabatico)}
+        , function(){ alertify.error('Se cancelo')});
+}
+function eliminarDatos(id_sabatico) {
+    cadena= "id_sabatico=" + id_sabatico;
+
+    $.ajax({
+        type:"post",
+        url:"assets/components/PHP_Consultas/Registro_Producto_Anio_Sabatico/Eliminar_Registro.php",
+        data:cadena,
+        success:function (r) {
+            if(r==1){
+                $('#registro-anio-sabatico').load('assets/components/registro-anio-sabatico.php');
+                alertify.success("Eliminado con exito!");
+            }else{
+                alertify.error("Fallo el servidor!");
+            }
+        }
+    });
+}
