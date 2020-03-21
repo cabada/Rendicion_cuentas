c13de8646ca8d1e2138b2857ad2e1a25d28fc592
¨Cabada¨ <j_cabada@hotmail.com>
2020-03-20 22:17:15 -0600

    Se modifico el query.

--- assets/components/PHP_Consultas/Registro_Profesores_Tiempo_Parcial/Agregar_Registro.php
+++ assets/components/PHP_Consultas/Registro_Profesores_Tiempo_Parcial/Agregar_Registro.php
@@ -7,7 +7,7 @@ $cantidad_tiempo_parcial=$_POST['cantidad_tiempo_parcial'];
 $grado=$_POST['grado'];
 
 $stmt = $conexion->prepare("insert into profesores_tiempo_parcial (cantidad_tiempo_parcial,grado) values (?,?)");
-$stmt->bind_param("ssss", $cantidad_tiempo_parcial,$grado);
+$stmt->bind_param("ss", $cantidad_tiempo_parcial,$grado);
 
 echo $resultado = $stmt->execute();
 $stmt->close();
