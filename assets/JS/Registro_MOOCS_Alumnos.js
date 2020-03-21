72b980018118a19971d35cd207c086e310ba56ac
¨Cabada¨ <j_cabada@hotmail.com>
2020-03-21 03:02:03 -0600

    Se agregaron el archivo JS para la agregacion, eliminacion y actualizacion de la tabla moocs alumnos.

---
+++ assets/JS/Registro_MOOCS_Alumnos.js
@@ -0,0 +1,81 @@
+function  agregarDatos(nombre_curso,numero_alumnos) {
+
+    cadena="cursos_mooc=" + nombre_curso +
+        "&numero_alumnos_inscritos=" + numero_alumnos;
+
+    $.ajax({
+        type: "POST",
+        url: "assets/components/PHP_Consultas/Registro_MOOCS_Alumnos/Agregar_Registro.php",
+        data: cadena,
+        success: function (r) {
+            if (r == 1) {
+                $('#registro-cantidad-alumnos-moocs').load('assets/components/registro-cantidad-alumnos-moocs.php');
+                alertify.success("Agregado con exito");
+            } else {
+                alertify.error("Fallo el servidor");
+            }
+        }
+    });
+}
+
+function agregaform(datos) {
+    d=datos.split('||');
+
+    $('#id_moocs_alm_editar').val(d[0]);
+    $('#nombre_curso_editar').val(d[1]);
+    $('#cantidad_alumnos_editar').val(d[2]);
+
+}
+
+function actualizarDatos() {
+    id_moocs_alumnos=parseInt($('#id_moocs_alm_editar').val());
+    console.log(id_moocs_alumnos);
+    nombre_curso= $('#nombre_curso_editar').val();
+    console.log(nombre_curso);
+    numero_alumnos=parseInt($('#cantidad_alumnos_editar').val());
+    console.log(numero_alumnos);
+
+
+    cadena="id_moocs_alumnos="+id_moocs_alumnos+
+        "&cursos_mooc=" + nombre_curso+
+        "&numero_alumnos_inscritos=" + numero_alumnos;
+
+    $.ajax({
+        type: "POST",
+        url: "assets/components/PHP_Consultas/Registro_MOOCS_Alumnos/Actualizar_Registro.php",
+        data: cadena,
+        success: function (r) {
+            if (r == 1) {
+                $('#registro-cantidad-alumnos-moocs').load('assets/components/registro-cantidad-alumnos-moocs.php');
+                alertify.success("Actualizado con exito");
+            } else {
+                alertify.error("Fallo el servidor");
+            }
+        }
+    });
+
+}
+
+function preguntarSiNo(id_moocs_alm){
+
+    alertify.confirm('Eliminar este registro', 'Esta seguro de eliminar este registro?',
+        function(){ eliminarDatos(id_moocs_alm)}
+        , function(){ alertify.error('Se cancelo')});
+}
+function eliminarDatos(id_moocs_alm) {
+    cadena= "id_moocs_alumnos=" + id_moocs_alm;
+
+    $.ajax({
+        type:"post",
+        url:"assets/components/PHP_Consultas/Registro_MOOCS_Alumnos/Eliminar_Registro.php",
+        data:cadena,
+        success:function (r) {
+            if(r==1){
+                $('#registro-cantidad-alumnos-moocs').load('assets/components/registro-cantidad-alumnos-moocs.php');
+                alertify.success("Eliminado con exito!");
+            }else{
+                alertify.error("Fallo el servidor!");
+            }
+        }
+    });
+}
--- assets/components/registro-cantidad-alumnos-moocs.php
+++ assets/components/registro-cantidad-alumnos-moocs.php
@@ -1,3 +1,12 @@
+<?php
+
+require_once "PHP_Consultas/Conexion.php";
+$conexion = conexion();
+
+?>
+
+
+
 <div class="row">
     <div class="col-sm-12">
         <h2>Registro de cantidad de alumnos en cursos de moocs</h2>
@@ -11,15 +20,30 @@
                     <td class="text-center align-middle background-table">Cantidad de alumnos inscritos</td>
                     <td class="text-center align-middle background-table">Acciones</td>
                 </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td class="text-center align-middle">
-                        <button class="btn btn-sm btn-warning" data-toggle="modal" data-target="#modalEdicion"><i class="far fa-edit"></i>  Editar</button>
-                        <button class="btn btn-sm btn-danger"><i class="fas fa-trash"></i> Eliminar</button>
-                    </td>
-                    
-                </tr>
+                <?php
+                $sql="select id_moocs_alumnos,cursos_mooc,numero_alumnos_inscritos
+                            from moocs_alumnos";
+                $result=mysqli_query($conexion,$sql);
+
+                while($buscar=mysqli_fetch_row($result)){
+
+                    $datos=$buscar[0]."||".
+                        $buscar[1]."||".
+                        $buscar[2];
+                    ?>
+
+                    <tr>
+                        <td><?php echo $buscar[1]?></td>
+                        <td><?php echo $buscar[2]?></td>
+
+                        <td class="text-center align-middle">
+                            <button class="btn btn-sm btn-warning" data-toggle="modal" data-target="#modalEdicion" onclick="agregaform('<?php echo $datos ?>')"><i class="far fa-edit"></i>  Editar</button>
+                            <button class="btn btn-sm btn-danger" onclick="preguntarSiNo('<?php echo $buscar[0]?>')"><i class="fas fa-trash" ></i>  Eliminar</button>
+                        </td>
+                    </tr>
+                    <?php
+                }
+                ?>
             </table>
         </div>
     </div>
