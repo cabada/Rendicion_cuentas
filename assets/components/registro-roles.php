9d37654b04411440d1a984c48ec233ce373b5f90
¨Cabada¨ <j_cabada@hotmail.com>
2020-04-10 01:08:58 -0600

    Se agrego el PHP para guardar los roles y los permisos.

--- assets/components/registro-roles.php
+++ assets/components/registro-roles.php
@@ -20,21 +20,37 @@ $conexion = conexion();
                     <td class="text-center align-middle background-table">Acciones</td>
                 </tr>
                 <?php
+                $query = "select distinct roles.id_rol,
+                                    roles.nombre_rol,
+                                    operaciones.nombre_operacion,
+                                    modulo.nombre_modulo 
+                                    from roles
+                                    join rol_operacion
+                                    on roles.id_rol=rol_operacion.id_rol
+                                    join operaciones
+                                    on operaciones.id_operaciones=rol_operacion.id_operacion
+                                    join modulo
+                                    on modulo.id_modulo = operaciones.id_modulo";
+                $resultado = mysqli_query($conexion,$query);
 
-                    ?>
+                while($fila = mysqli_fetch_array($resultado)){
+                    $valor = $fila['nombre_modulo'];
+
+                ?>
+
 
                     <tr>
-                        <td><?php ?></td>
-                        <td><?php ?></td>
-                        <td><?php /*echo*/ /*$buscar[4]*/?></td>
+                        <td><?php echo "<label>".$fila[1]."</label>"?></td>
+                        <td><?php echo "<label>".$fila[2]."</label>"?></td>
+                        <td><?php echo "<label>".$fila['nombre_modulo']."</label>"?></td>
                         <td class="text-center align-middle">
-                            <button class="btn btn-sm btn-warning" data-toggle="modal" data-target="#modalEdicion" <!--onclick="agregaform('<?php /*echo $datos */?>')"-->><i class="far fa-edit"></i>  Editar</button>
-                            <button class="btn btn-sm btn-danger" <!--onclick="preguntarSiNo('<?php /*echo $buscar[0]*/?>')"-->><i class="fas fa-trash" ></i>  Eliminar</button>
+                            <button class="btn btn-sm btn-warning" data-toggle="modal" data-target="#modalEdicion" <!--onclick="agregaform('<?php /*echo $datos */?>')"--><i class="far fa-edit"></i>  Editar</button>
+                            <button class="btn btn-sm btn-danger" <!--onclick="preguntarSiNo('<?php /*echo $buscar[0]*/?>')"--><i class="fas fa-trash" ></i>  Eliminar</button>
                         </td>
                     </tr>
-                   <!-- --><?php
-/*                }
-                */?>
+                   <?php
+               }
+                ?>
             </table>
         </div>
 
