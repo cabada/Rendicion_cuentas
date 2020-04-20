618eb8f68e52a6c334175f2061e66f58010c6b1e
¨Cabada¨ <j_cabada@hotmail.com>
2020-04-19 18:38:46 -0600

    Se verifico que los usuarios pudieran ver las tabla cuerpos academicos y cuerpos academicos posgrado.

--- assets/components/registro-cuerpos-academicos.php
+++ assets/components/registro-cuerpos-academicos.php
@@ -1,7 +1,23 @@
 <?php
 
 require_once "PHP_Consultas/Conexion.php";
+require_once "PHP_Consultas/Usuarios/Verificar_Tablas_Usuarios.php";
 $conexion = conexion();
+$conn = conexion();
+session_start();
+$id_usuario = $_SESSION["id_usuario"];
+$stmt = consultaTablas($conn,$id_usuario);
+
+
+$stmt->execute();
+
+$stmt->bind_result($resultado);
+
+while($stmt->fetch()){
+
+$tablaRequerida = 'cuerpos_academicos';
+
+if($resultado == $tablaRequerida){
 
 ?>
 
@@ -69,3 +85,17 @@ $conexion = conexion();
         </div>
     </div>
 </div>
+
+
+    <?php
+}
+
+
+}
+
+$stmt->close();
+$conexion->close();
+
+
+
+?>
