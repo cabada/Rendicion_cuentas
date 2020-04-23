47822e422128b1b89ef634763fb73cc5ed929ae2
iProxys <edwinavahh@outlook.com>
2020-04-22 22:39:35 -0600

    cambios

--- assets/components/PHP_Consultas/Usuarios/Verificar_Usuario.php
+++ assets/components/PHP_Consultas/Usuarios/Verificar_Usuario.php
@@ -7,7 +7,6 @@ $conexion = conexion();
 $correo_electronico = $_POST['correo_electronico'];
 $contrasena = $_POST['contrasena'];
 
-
 $stmt = $conexion->prepare("select id_usuario from usuarios where email_usuario=? and contrasena_usuario=?");
 $stmt->bind_param("ss", $correo_electronico, $contrasena);
 
