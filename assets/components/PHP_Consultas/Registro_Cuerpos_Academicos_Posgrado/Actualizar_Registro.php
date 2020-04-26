fd0af4b45c0b360f8ec2393a9dca0b5f01d0a024
ArelyAM <56061706+ArelyAM@users.noreply.github.com>
2020-04-25 22:30:58 -0600

    Se elimino columna area academica

--- assets/components/PHP_Consultas/Registro_Cuerpos_Academicos_Posgrado/Actualizar_Registro.php
+++ assets/components/PHP_Consultas/Registro_Cuerpos_Academicos_Posgrado/Actualizar_Registro.php
@@ -13,7 +13,6 @@ $stmt->execute();
 
 if($stmt->fetch()){
 $id_cuerpos_academicos_posgrado=$_POST['id_cuerpos_academicos_posgrado'];
-$area_academica=$_POST['area_academica'];
 $nombre_cuerpo=$_POST['nombre_cuerpo'];
 $grado=$_POST['grado'];
 $estado=$_POST['estado'];
@@ -22,7 +21,6 @@ $vigencia=$_POST['vigencia'];
 $area=$_POST['area'];
 
 $stmt = $conexion->prepare("update cuerpos_academicos_posgrado set
-                                   AREA_ACADEMICA=?,
                                    NOMBRE_CUERPO=?,
                                    GRADO=?,
                                    ESTADO=?,
@@ -31,7 +29,7 @@ $stmt = $conexion->prepare("update cuerpos_academicos_posgrado set
                                    AREA=? 
                                    where ID_CUERPOS_ACADEMICOS_POSGRADO=$id_cuerpos_academicos_posgrado");
 
-$stmt->bind_param("ssssiss", $area_academica,$nombre_cuerpo,$grado,$estado,$anio_registro,$vigencia,$area);
+$stmt->bind_param("sssiss", $nombre_cuerpo,$grado,$estado,$anio_registro,$vigencia,$area);
 
 echo $resultado = $stmt->execute();
 $stmt->close();
--- assets/components/PHP_Consultas/Registro_Cuerpos_Academicos_Posgrado/Agregar_Registro.php
+++ assets/components/PHP_Consultas/Registro_Cuerpos_Academicos_Posgrado/Agregar_Registro.php
@@ -14,7 +14,6 @@ $query = consultaPermisos($conn,$id_usuario,$tabla,'Insert');
 $query->execute();
 
 if($query->fetch()) {
-$area_academica=$_POST['area_academica'];
 $nombre_cuerpo=$_POST['nombre_cuerpo'];
 $grado=$_POST['grado'];
 $estado=$_POST['estado'];
@@ -22,8 +21,8 @@ $anio_registro=$_POST['anio_registro'];
 $vigencia=$_POST['vigencia'];
 $area=$_POST['area'];
 
-$stmt = $conexion->prepare("insert into cuerpos_academicos_posgrado (AREA_ACADEMICA, NOMBRE_CUERPO, GRADO, ESTADO, ANIO_REGISTRO, VIGENCIA, AREA) values (?,?,?,?,?,?,?)");
-$stmt->bind_param("ssssiss", $area_academica,$nombre_cuerpo,$grado,$estado,$anio_registro,$vigencia,$area );
+$stmt = $conexion->prepare("insert into cuerpos_academicos_posgrado (NOMBRE_CUERPO, GRADO, ESTADO, ANIO_REGISTRO, VIGENCIA, AREA) values (?,?,?,?,?,?,?)");
+$stmt->bind_param("sssiss", $nombre_cuerpo,$grado,$estado,$anio_registro,$vigencia,$area );
 
 echo $resultado = $stmt->execute();
 $stmt->close();
