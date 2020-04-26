418a0d775f7bedf2ae4b723d2dc504cc682943c0
¨Cabada¨ <j_cabada@hotmail.com>
2020-04-25 23:21:00 -0600

    Se quito el porcentaje en la actualizacion del programa posgrado.

--- assets/components/PHP_Consultas/Registro_Total_Alumnos_Programa_Posgrado/Actualizar_Registro.php
+++ assets/components/PHP_Consultas/Registro_Total_Alumnos_Programa_Posgrado/Actualizar_Registro.php
@@ -21,10 +21,9 @@ $registrado_en=$_POST['registrado_en'];
 $stmt = $conexion->prepare("update total_alumnos_programa_posgrado set
                                   PROGRAMA=?,
                                   CANTIDAD=?,
-                                  PORCENTAJE=?,
                                   REGISTRADO_EN=?
                                   where ID_TOTAL_PROG_POSGRADO=$id_total_prog_posgrado");
-$stmt->bind_param("siss",$programa,$cantidad,$porcentaje,$registrado_en);
+$stmt->bind_param("sis",$programa,$cantidad,$porcentaje,$registrado_en);
 
 echo $resultado = $stmt->execute();
 $stmt->close();
