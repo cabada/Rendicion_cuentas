8e4ac21883a6c7ea7aba86c33dfa26071d2eeb5c
ArelyAM <56061706+ArelyAM@users.noreply.github.com>
2020-04-25 22:02:10 -0600

    Se agrego join para relacionar id_carrera

---
+++ assets/components/PHP_Consultas/Registro_Permanencia/modal_permanencia.php
@@ -0,0 +1,100 @@
+<?php
+
+require_once "../Conexion.php";
+$conexion = conexion();
+
+?>
+
+<!-- MODAL FOR NEW FORM -->
+<div class="modal fade" id="new-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="new-modalLabel">Agregar nuevo registro</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <label>Nombre de programa</label>
+                <select type="text" id="nombre_programa_agregar" class="form-control-page input-group-sm">
+                    <?php
+
+                    $query = "select id_carrera,nombre_carrera from carreras where id_programa_universi=1";
+                    $resultado = mysqli_query($conexion,$query);
+
+                    while($fila = mysqli_fetch_array($resultado)){
+                        $valor = $fila['nombre_carrera'];
+
+                        echo "<option value=\"".$fila['id_carrera']."\">".$fila['nombre_carrera']."</option>\n";
+                    }
+                    ?>
+                </select>
+
+                <label>Porcentaje</label>
+                <input type="text" id="porcentaje_agregar" class="form-control-page input-group-sm">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-main" data-dismiss="modal" id="btn_agregar_curso_actual">Agregar Nuevo Registro</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- MODAL FOR EDITION -->
+<div class="modal fade" id="modalEdicion" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Editar registro</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <label>Numero de registro</label>
+                <input type="id" id="id_permanencia" class="form-control-page input-group-sm" readonly="readonly">
+
+                <label>Nombre de programa</label>
+                <select type="text" id="nombre_programa_editar" class="form-control-page input-group-sm">
+                    <?php
+                    $query = "select id_carrera,nombre_carrera from carreras where id_programa_universi=1";
+                    $resultado = mysqli_query($conexion,$query);
+
+                    while($fila = mysqli_fetch_array($resultado)){
+                        $valor = $fila['nombre_carrera'];
+
+                        echo "<option value=\"".$fila['id_carrera']."\">".$fila['nombre_carrera']."</option>\n";
+
+                    }
+                    ?>
+                </select>
+
+                <label>Porcentaje</label>
+                <input type="text" id="porcentaje_editar" class="form-control-page input-group-sm">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-main" data-dismiss="modal" id="btn_editar_curso_actual">Guardar cambios</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#btn_agregar_curso_actual').click(function () {
+
+            programa=$('#nombre_programa_agregar').val();
+            console.log(programa);
+            porcentaje=$('#porcentaje_agregar').val();
+            console.log(porcentaje);
+
+            agregarDatos(programa,porcentaje);
+
+        });
+        $('#btn_editar_curso_actual').click(function () {
+            actualizaDatos();
+        });
+    });
+</script>
