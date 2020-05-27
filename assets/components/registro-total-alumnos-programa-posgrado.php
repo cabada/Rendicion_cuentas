e0553868678116de89eb898464311ac2a01f8b46
ArelyAM <56061706+ArelyAM@users.noreply.github.com>
2020-05-26 22:18:29 -0600

    Librerias

--- assets/components/registro-total-alumnos-programa-posgrado.php
+++ assets/components/registro-total-alumnos-programa-posgrado.php
@@ -38,8 +38,7 @@ if($resultado == $tablaRequerida){
                     <td class="text-center align-middle background-table">Registrado en</td>
                     <td class="text-center align-middle background-table">Acciones</td>
                   </tr>
-                </thead>
-                <tbody>
+
                 <?php
                 $sql="select total_alumnos_programa_posgrado.ID_TOTAL_PROG_POSGRADO,
                             carreras.nombre_carrera,
@@ -96,7 +95,6 @@ if($resultado == $tablaRequerida){
                     <td>100%</td>
 
                 </tr>
-                </tbody>
             </table>
         </div>
     </div>
@@ -115,42 +113,3 @@ $conexion->close();
 
 ?>
 
-<script type="text/javascript">
-    $(document).ready(function () {
-        $('#Tabla').DataTable({
-            dom: 'Brtip',
-            buttons: [
-                'copyHtml5',
-                'excelHtml5',
-                'csvHtml5',
-                'pdfHtml5'
-            ],
-            language:{
-                "sProcessing":     "Procesando...",
-                "sLengthMenu":     "Mostrar _MENU_ registros",
-                "sZeroRecords":    "No se encontraron resultados",
-                "sEmptyTable":     "Ningún dato disponible en esta tabla",
-                "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
-                "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
-                "sInfoPostFix":    "",
-                "sSearch":         "Buscar:",
-                "sUrl":            "",
-                "sInfoThousands":  ",",
-                "sLoadingRecords": "Cargando...",
-                "oPaginate": {
-                    "sFirst":    "Primero",
-                    "sLast":     "Último",
-                    "sNext":     "Siguiente",
-                    "sPrevious": "Anterior"
-                },
-                "oAria": {
-                    "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
-                    "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-                }
-            }
-cc
-        });
-    });
-
-</script>
