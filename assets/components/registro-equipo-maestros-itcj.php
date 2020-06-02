f0c584fcbbd993cab3ab285645d31deed88c1e60
¨Cabada¨ <j_cabada@hotmail.com>
2020-06-02 16:22:56 -0600

    Se hizo la exportacion de excel y el buscador y el buscador por fecha de la tabla equipo de maestros itcj

--- assets/components/registro-equipo-maestros-itcj.php
+++ assets/components/registro-equipo-maestros-itcj.php
@@ -21,15 +21,98 @@ $tablaRequerida = 'equipo_maestros_itcj';
 if($resultado == $tablaRequerida){
 
 ?>
+    <div class="row">
+        <div class="col-sm-12">
 
-<div class="row">
+            <!--Botones Excel y PDF -->
+            <div class="row mt-2">
+                <div class="col-12">
+                    <form id="reporte" name="reporte" method="POST" target="_blank">
+                        <div class="form-group">
+                            <div class="form-row d-flex">
+                                <div class="col">
+                                    <input class="btn btn-danger text-white" type="button" target="_blank" value="Exportar PDF"
+                                           onclick= "createPDF()" />
+
+
+                                    <!--document.reporte.action = 'assets/components/PHP_Consultas/Registro_Total_Alumnos_Programa_Posgrado/reportePDF.php';
+                                    document.reporte.submit()-->
+
+                                    <input class="btn btn-success text-white" type="button" value="Exportar Excel"
+                                           onclick= "tableToExcel('tabla', 'Listado de Maestros con Certificaciones')" />
+
+                                </div>
+
+                                <!--Select de anio-->
+                                <div class="col d-flex justify-content-end">
+                                    <select class="form-control col-md-5 anio" id="anio-select" name="anio-select">
+                                        <option>Buscar por año</option>
+                                        <?php
+                                        $query = "select distinct year(fecha_creado) as fecha_creado from equipo_maestros_itcj order by fecha_creado desc";
+                                        $resultado = mysqli_query($conexion,$query);
+
+                                        while($fila = mysqli_fetch_array($resultado)){
+                                            $valor = $fila['nombre_area_academica'];
+
+                                            echo "<option>".($fila['fecha_creado'])."</option>\n";
+
+                                        }
+                                        ?>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+
+
+
+
+
+
+            <div class="row">
     <div class="col-sm-12">
-        <h2>Registro equipo de maestros ITCJ</h2>
-        <caption>
-            <button class="btn btn-main" data-toggle="modal" data-target="#new-modal">Agregar registro  <i class="fas fa-plus"></i></button>
-        </caption>
         <div class="table-responsive-xl">
-            <table class="table table-sm table-hover table-condensed table-bordered table-striped mt-2">
+
+                <?php
+                $salida = "";
+                $sql="select id_equipo_maestros_itcj,nombre_docente,categoria_hora,grado_estudios,sni,area_especializacion,
+                experiencia_profesional,experiencia_docente from equipo_maestros_itcj";
+
+                if(isset($_POST['consulta'])){
+                    $q = $conexion->real_escape_string($_POST['consulta']);
+                    $sql = "select id_equipo_maestros_itcj,
+                                    nombre_docente,
+                                    categoria_hora,
+                                    grado_estudios,
+                                    sni,area_especializacion,
+                                    experiencia_profesional,
+                                    experiencia_docente 
+                                    from equipo_maestros_itcj
+                                    where nombre_docente like '%$q%' or grado_estudios like '%$q%' or grado_estudios like '%$q%'";
+                }
+
+                if(isset($_POST['consulta_anio'])){
+                    $q = $conexion->real_escape_string($_POST['consulta_anio']);
+                    $sql = "select id_equipo_maestros_itcj,
+                                    nombre_docente,
+                                    categoria_hora,
+                                    grado_estudios,
+                                    sni,area_especializacion,
+                                    experiencia_profesional,
+                                    experiencia_docente 
+                                    from equipo_maestros_itcj
+                                    where fecha_creado like '%$q%'";
+
+                }
+
+                $resultado = $conexion->query($sql);
+                if($resultado->num_rows>0){
+
+                    $salida.='
+                     <table class="table table-sm table-hover table-condensed table-bordered table-striped mt-2">
                 <tr>
                     <td class="text-center align-middle background-table">Nombre de docente</td>
                     <td class="text-center align-middle background-table">Categoría</td>
@@ -39,12 +122,8 @@ if($resultado == $tablaRequerida){
                     <td class="text-center align-middle background-table">Experiencia profesional</td>
                     <td class="text-center align-middle background-table">Experiencia docente</td>
                     <td class="text-center align-middle background-table">Acciones</td>
-                </tr>
+                </tr>';
 
-                <?php
-
-                $sql="select id_equipo_maestros_itcj,nombre_docente,categoria_hora,grado_estudios,sni,area_especializacion,
-                experiencia_profesional,experiencia_docente from equipo_maestros_itcj";
 
                 $resultado = mysqli_query($conexion,$sql);
 
@@ -59,30 +138,51 @@ if($resultado == $tablaRequerida){
                     $buscar[6]."||".
                     $buscar[7];
 
-                ?>
 
+
+                $salida.='
                 <tr>
-                    <td><?php echo $buscar[1]?></td>
-                    <td><?php echo $buscar[2]?></td>
-                    <td><?php echo $buscar[3]?></td>
-                    <td><?php echo $buscar[4]?></td>
-                    <td><?php echo $buscar[5]?></td>
-                    <td><?php echo $buscar[6]?></td>
-                    <td><?php echo $buscar[7]?></td>
-                    <td class="text-center align-middle">
-                    <button class="btn btn-warning" onclick="agregaform('<?php echo $datos?>')" data-toggle="modal" data-target="#modalEdicion"><i class="far fa-edit"></i></button>
-                    <button class="btn btn-danger" onclick="preguntarSiNo('<?php echo $buscar[0]?>')"><i class="far fa-window-close"></i></button>
-                    </td>
-                </tr>
+                    <td>'.$buscar[1].'</td>
+                    <td>'.$buscar[2].'</td>
+                    <td>'.$buscar[3].'</td>
+                    <td>'.$buscar[4].'</td>
+                    <td>'.$buscar[5].'</td>
+                    <td>'.$buscar[6].'</td>
+                    <td>'.$buscar[7].'</td>
+                     <td class="text-center align-middle">
+                            <button class="btn btn-sm btn-warning" data-toggle="modal" data-target="#modalEdicion"  onclick="agregaform(\''.$datos.'\')" ><i class="far fa-edit"></i>  Editar</button>
+                            <button class="btn btn-sm btn-danger" onclick="preguntarSiNo(\''.$buscar[0].'\')"><i class="fas fa-trash"></i>  Eliminar</button>
+                                    
+                     </td>
+                </tr>';
 
 
-                    <?php
+
                 }
                 ?>
             </table>
+                    <?php
+                    }else{
+                    $salida.='<div class="row mt-3">
+                            <div class="col-12 text-center">
+                                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                    <strong>¡No se encontró ningún elemento!</strong><br>
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>';
+
+                }
+                echo $salida;
+
+                    ?>
         </div>
     </div>
 </div>
+        </div>
+    </div>
 
     <?php
 }
@@ -96,3 +196,21 @@ $conexion->close();
 
 
 ?>
+
+<script>
+
+    var tableToExcel = (function() {
+        var uri = 'data:application/vnd.ms-excel;base64,'
+            , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
+            , base64 = function(s) { return window.btoa(unescape(encodeURIComponent(s))) }
+            , format = function(s, c) { return s.replace(/{(\w+)}/g, function(m, p) { return c[p]; }) }
+        return function(table, name) {
+            if (!table.nodeType) table = document.getElementById(table)
+            var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML}
+            window.location.href = uri + base64(format(template, ctx))
+        }
+    })()
+
+
+</script>
+
