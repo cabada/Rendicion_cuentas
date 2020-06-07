7d87ef4732840e6242abe0d5b51514f550ee8651
¨Cabada¨ <j_cabada@hotmail.com>
2020-06-06 23:45:26 -0600

    Se arreglo el bug de la tabla docentes con reconocimiento de perfil deseable

--- assets/components/PHP_Consultas/Registro_Maestros_Certificaciones/reportePDF.php
+++ assets/components/PHP_Consultas/Registro_Maestros_Certificaciones/reportePDF.php
@@ -63,7 +63,9 @@ public function Footer()
     $pdf->SetLineWidth(1);
 
 
+/*Verifica si la variable global fue definida*/
 if(isset($_SESSION['consulta'])){
+    /*Se le pasa el valor de la variable global a $q*/
     $q = $_SESSION['consulta'];
     $sql="select profesores.id_profesor,
                             profesores.nombre_completo,
@@ -74,11 +76,35 @@ if(isset($_SESSION['consulta'])){
                             on area_academica.id_area_academica = profesores.id_area_academica
                             where profesores.id_categoria_profesores = 1
                             and (profesores.nombre_completo like '%$q%' or area_academica.nombre_area_academica like '%$q%')";
+
+
+    if(isset($_SESSION['consulta_anio'])){
+
+        $p = $_SESSION['consulta_anio'];
+        $sql="select profesores.id_profesor,
+                            profesores.nombre_completo,
+                            area_academica.nombre_area_academica,
+                            profesores.disciplina
+                            from profesores
+                            join area_academica
+                            on area_academica.id_area_academica = profesores.id_area_academica
+                            where profesores.id_categoria_profesores = 1
+                            and (profesores.nombre_completo like '%$q%' or area_academica.nombre_area_academica like '%$q%')
+                            and profesores.fecha_creado like '%$p%'";
+
+    }
+    /*Se destruye/quita el valor dentro de la variable global*/
     unset($_SESSION['consulta']);
+    unset($_SESSION['consulta_anio']);
 
 }
+
+/*Sino se cumple el if de arriba, se pasa a este.
+Verifica si la variable global fue definida*/
 elseif (isset($_SESSION['consulta_anio'])){
+    /*Se le pasa el valor de la variable global a $q*/
     $q = $_SESSION['consulta_anio'];
+
     $sql="select profesores.id_profesor,
                             profesores.nombre_completo,
                             area_academica.nombre_area_academica,
@@ -87,8 +113,11 @@ elseif (isset($_SESSION['consulta_anio'])){
                             join area_academica
                             on area_academica.id_area_academica = profesores.id_area_academica
                             where profesores.id_categoria_profesores = 1 and profesores.fecha_creado like '%$q%'";
+    /*Se destruye/quita el valor dentro de la variable global*/
     unset($_SESSION['consulta_anio']);
 }
+/*Sino se cumplio ninguno de arriba, se va a ejecutar esta instruccion que es la de por defecto. Es una query para ver todos los registros
+de la tabla.*/
 else{
 
     $sql="select profesores.id_profesor,
@@ -99,6 +128,8 @@ else{
                             join area_academica
                             on area_academica.id_area_academica = profesores.id_area_academica
                             where profesores.id_categoria_profesores = 1";
+    unset($_SESSION['consulta']);
+    unset($_SESSION['consulta_anio']);
 
 }
 
