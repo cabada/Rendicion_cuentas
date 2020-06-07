becbfe62d26b3f29cc0318aa0b4facc9c7934b59
¨Cabada¨ <j_cabada@hotmail.com>
2020-06-06 23:37:35 -0600

    Se arreglo el bug de la tabla maestros con certificaciones

--- assets/JS/Registro_Maestros_Certificaciones.js
+++ assets/JS/Registro_Maestros_Certificaciones.js
@@ -161,10 +161,14 @@ function buscar_datos_anio(consulta_anio){
 $(document).on('change','.anio', function(){
 
 
-   var valor = $(this).val();
-    if (valor != "") {
+    var valor = $(this).val();
+    if (valor != "Todos los registros") {
         buscar_datos_anio(valor);
-    }else{
+
+    }
+    else{
         buscar_datos_anio();
+        $('#caja_busqueda').val('');
+        buscar_datos("");
     }
 });
